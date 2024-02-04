26a0edc1b34240e69d4dc216d9d5d7d1d7b6eace
Cesario54 <mincesario548@gmail.com>
2024-02-04 14:52:16 -0600

    iniciamos

---
+++ src/figuras/Figuras2D.java
@@ -0,0 +1,17 @@
+package figuras;
+
+public class Figuras2D{
+    int[] posicion=new int[2];
+    private String color;
+
+    public Figuras2D (int posicion[], String color){
+        this.posicion[0]=posicion[0];
+        this.posicion[1]=posicion[1];
+        this.color=color;
+    }
+
+    public String getcolor(){
+        return color;
+    }
+
+}
